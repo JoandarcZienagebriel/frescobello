0430c2fd8aa737ff4063faf95788a6d35c94c46c
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-07-14 08:29:18 +0300

    change-btn-label

--- api/order.js
+++ api/order.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { Resend } from "resend";
-
+console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY);
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const supabase = createClient(
