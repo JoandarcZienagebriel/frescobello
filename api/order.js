67378b3f5902008d456830e605cd8f5a0365c8e4
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-07-15 14:58:02 +0300

    testing-resend

--- api/order.js
+++ api/order.js
@@ -1,14 +1,20 @@
 import { createClient } from "@supabase/supabase-js";
-import { Resend } from "resend";
-console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY);
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
+
+
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
 
 export default async function handler(req, res) {
 
@@ -92,29 +98,56 @@ export default async function handler(req, res) {
       throw error;
 
     }
-    console.log("Resend key exists:", !!process.env.RESEND_API_KEY);
-
+    console.log("Gmail exists:", !!process.env.GMAIL_USER);
 try {
-  const { data: emailData, error: emailError } = await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "pedromagri850@gmail.com",
+
+  await transporter.sendMail({
+
+    from: process.env.GMAIL_USER,
+
+    to: "joandarczienagebriel@gmail.com",
+
     subject: `New Order ${orderNumber}`,
+
     html: `
-      <h2>New Order</h2>
-      <p>Order Number: ${orderNumber}</p>
-      <p>Customer: ${customer.name}</p>
-      <p>Total: €${totals.total}</p>
-    `,
+      <h2>New Order Received</h2>
+
+      <p>
+      Order Number:
+      ${orderNumber}
+      </p>
+
+      <p>
+      Customer:
+      ${customer.name}
+      </p>
+
+      <p>
+      Phone:
+      ${customer.phone}
+      </p>
+
+      <p>
+      Address:
+      ${customer.address}
+      </p>
+
+      <p>
+      Total:
+      €${totals.total}
+      </p>
+    `
+
   });
 
-  console.log("Email data:", emailData);
-  console.log("Email error:", emailError);
 
-  if (emailError) {
-    throw new Error(JSON.stringify(emailError));
-  }
-} catch (err) {
-  console.error("Resend failed:", err);
+  console.log("Email sent");
+
+
+} catch(error){
+
+  console.error("Email error:", error);
+
 }
 
 // 👇 THEN return the response
@@ -124,13 +157,6 @@ return res.status(200).json({
 });
 
 
-    return res.status(200).json({
-
-      success:true,
-
-      order:data
-
-    });
 
 
 
